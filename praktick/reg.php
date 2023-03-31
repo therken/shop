9e1cb0a03db3a0e44d18e88e97af97f8d8fbc19b
kaguhe <107109714+therken@users.noreply.github.com>
2023-03-31 07:08:04 +0300

    Add files via upload

---
+++ praktick/reg.php
@@ -0,0 +1,36 @@
+<?php
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+$db = 'form';
+
+$conn = mysqli_connect($host, $user, $pass, $db);
+if ($conn === false) {
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+
+// Использование подготовленных выражений
+$stmt = mysqli_prepare($conn, "INSERT INTO form (email, password) VALUES (?, ?)");
+if ($stmt === false) {
+    die("ERROR: Could not prepare statement. " . mysqli_error($conn));
+}
+
+$mail = $_POST['mail'];
+$password = $_POST['psw'];
+
+// Использование md5 для хеширования пароля
+$hashed_password = md5($password);
+
+mysqli_stmt_bind_param($stmt, 'ss', $mail, $hashed_password);
+
+if (mysqli_stmt_execute($stmt)) {
+    mysqli_stmt_close($stmt);
+    mysqli_close($conn);
+    echo '<script>alert("Информация добавлена успешно."); window.location.href="reg.html";</script>';
+    exit;
+} else {
+    echo "Ошибка: " . mysqli_error($conn);
+    mysqli_stmt_close($stmt);
+    mysqli_close($conn);
+}
+?>
---
+++ praktick/script.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+	
+    var readURL = function(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                $('.profile-pic').attr('src', e.target.result);
+            }
+    
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+   
+    $(".file-upload").on('change', function(){
+        readURL(this);
+    });
+    
+    $(".upload-button").on('click', function() {
+       $(".file-upload").click();
+    });
+});
+
+const email = document.getElementById("mail");
+
+email.addEventListener("input", function (event) {
+  if (email.validity.typeMismatch) {
+    email.setCustomValidity("I am expecting an e-mail address!");
+  } else {
+    email.setCustomValidity("");
+  }
+});
