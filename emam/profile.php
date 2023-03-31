a67118040975fe71c1486eaad33b89c5349cb5e5
kaguhe <107109714+therken@users.noreply.github.com>
2023-03-31 09:27:37 +0300

    Update profile.php

--- emam/profile.php
+++ emam/profile.php
@@ -1,53 +1,77 @@
-<!DOCTYPE html>
-<html lang="ru">
-<head>
-<meta charset="UTF-8">
-<title>Редактирование профиля</title>
-<link rel="stylesheet" href="https://bootstraptema.ru/plugins/2015/bootstrap3/bootstrap.min.css" />
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-<link rel="stylesheet" href="./redactstyle.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
-<script src="redscript.js"></script>
-</head>
-<body>
-<div class="wrapper">
-<nav>
-<ul>
-<li><a href="../home.html">Главная</a></li>
-<li><a href="../profile.html">Назад</a></li>
-</ul>
-</nav>
-<div class="container">
-<div class="row mt-5">
-<div class="col-md-4 left-part">
-<form id="form" action="profile.php" method="POST" enctype="multipart/form-data">
-<div class="form-group text-center">
-<label type="file" for="profile-photo">
-<img src="https://preview.redd.it/imrpoved-steam-default-avatar-v0-ffxjnceu7vf81.png?width=640&crop=smart&auto=webp&s=0f8cbc4130a94fc83f19418f1a734209108c2a4b" class="rounded-circle" alt="Фото профиля">
-</label>
-<input name="image" type="file" class="form-control-file" id="profile-photo" style="display: none">
-<label for="image">Выберите фотографию</label>
-</div>
-</div>
-<div class="col-md-8 right-part">
-<div class="form-group">
-<input type="text" class="form-control" id="firstname" name="firstname" placeholder="Укажите ваш никнейм" required>
-</div>
-<div class="form-group">
-<input type="text" name="mail" type="email" class="form-control" id="mail" placeholder="Укажите ваш email" required>
-</div>
-<div class="form-group">
-<textarea required type="text" maxlength="1000" class="form-control" id="about" rows="3" name="about" placeholder="Немного о себе"></textarea>
-</div>
-<input type="submit" placeholder="Отправить" value="Сохранить">
-</div>
-</form>
-</div>
-</div>
-</div>
-</body>
-</html>
+<?php
+$host= 'localhost';
+$user = 'root';
+$pass = '';
+$db = 'form'; 
+$conn = mysqli_connect($host , $user , $pass , $db);
+// Check connection
+if($conn === false){
+  die("ERROR: Невозможно подключиться. "
+      . mysqli_connect_error());
+}
+///переменные
+$name = mysqli_real_escape_string($conn ,$_POST['firstname']);
+$mail = mysqli_real_escape_string($conn,$_POST['mail']);
+$about = mysqli_real_escape_string($conn,$_POST['about']);
+$edate=strtotime($_POST['date']); 
+$edate=date("Y-m-d",$edate);
+$startDate = date('Y-m-d', strtotime("01/01/1900"));
+$endDate = date('Y-m-d', strtotime("01/10/2024"));
+
+///проверка существования email-адреса
+$sql = "SELECT mail FROM user_form WHERE mail='$mail'";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+$errorMessage = "Этот email уже используется";
+echo "$errorMessage";
+} else {
+  ///проверка даты
+if (($edate >= $startDate) && ($edate <= $endDate)) {
+  echo "";
+} else {
+  $errorMessage = "некорректная дата";
+  echo "$errorMessage"; 
+die();
+}
+///загрузка файла 
+$uploaddir = 'uploads/';
+$tempFilePath = $_FILES['image']['tmp_name']; 
+$fileName = $_FILES['image']['name']; 
+$uploadFile = $uploaddir . uniqid() . '-' . basename($fileName); 
+
+if(!is_uploaded_file($_FILES['image']['tmp_name'])) {
+     $errorMessage = "Выберите изображение";
+     echo "$errorMessage";
+     die();
+} 
+
+//Проверка что это картинка
+if (!getimagesize($_FILES["image"]["tmp_name"])) {
+     $errorMessage = "Это не картинка...";
+     echo "$errorMessage";
+     die();
+}
+
+if (move_uploaded_file($tempFilePath, $uploadFile)) {
+    $successMessage = "Файл корректен и был успешно загружен.";
+    echo "$successMessage";
+} else {
+    $errorMessage = "Возможная атака с помощью файловой загрузки!";
+    echo "'$errorMessage";
+}
+$photo_link = $uploadFile; 
+///добавление значений в бд
+  $sql = "INSERT INTO redactprofile (firstname, mail, about, date,img)
+  VALUES ('$name', '$mail', '$about', '$edate','$photo_link')";
+  ///вывод сообщения о том что данные добавлены
+  if(mysqli_query($conn, $sql)){
+    $successMessage= "Информация добавлена.";  
+    echo "$successMessage";
+  } else{
+    $errorMessage= "Ошибка $sql. "
+        . mysqli_error($conn);
+        echo "$errormeMessage";
+  }
+}
+mysqli_close($conn);
+?>
