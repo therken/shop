a59cce4b8e763bb0e4854cfdc1f156d003c97a41
kaguhe <andreisichev123@gmail.com>
2023-04-13 06:49:16 +0300

    скрипт в одном файле

---
+++ sneakers/dist/Adidas/script.js
@@ -0,0 +1,14 @@
+document.getElementById('range-picker').addEventListener('click', function(e) {
+  var sizeList = document.getElementById('range-picker').children;
+  for (var i = 0; i <= sizeList.length - 1; i++) {
+    console.log(sizeList[i].classList);
+    if (sizeList[i].classList.contains('active')) {
+      sizeList[i].classList.remove('active');
+    }
+  }
+  e.target.classList.add('active');
+})
+
+function goBack() {
+  window.history.back();
+}
