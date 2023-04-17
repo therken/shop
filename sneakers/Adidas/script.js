7d7acd9d43e537e3130e085c9a9e36c0cd173332
1W3UxNt <120009117+1W3UxNt@users.noreply.github.com>
2023-04-17 12:56:10 +0600

    Add files via upload

---
+++ sneakers/Adidas/script.js
@@ -0,0 +1,20 @@
+document.getElementById('range-picker').addEventListener('click', function(e) {
+  var sizeList = document.getElementById('range-picker').children;
+  for (var i = 0; i <= sizeList.length - 1; i++) {
+    console.log(sizeList[i].classList);
+    if (sizeList[i].classList.contains('active')) {
+      sizeList[i].classList.remove('active');
+    }
+  }
+  e.target.classList.add('active');
+})
+
+
+function goBack() {
+  window.history.back();
+}
+const buyButton = document.querySelector('.buy');
+
+buyButton.addEventListener('click', function() {
+  window.location.href = '../../../card/card.html';
+});
