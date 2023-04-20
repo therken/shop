895284b8296b472cbce2dfcfa5463d3266f248f1
kaguhe <andreisichev123@gmail.com>
2023-04-20 09:47:29 +0300

    стили для редактирование профиля

--- profile/redact.php
+++ profile/redact.php
@@ -38,7 +38,7 @@ mysqli_close($conn);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Редактирование</title>
-    <link rel="stylesheet" src="./css/redstyle.css">
+    <link rel="stylesheet" href="./css/redstyle.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body>
