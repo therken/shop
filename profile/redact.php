51aa92d86f272512fcd0aef2661749a771281733
kaguhe <107109714+therken@users.noreply.github.com>
2023-04-20 12:26:19 +0300

    Update redact.php

--- profile/redact.php
+++ profile/redact.php
@@ -4,26 +4,26 @@ session_start();
 
 // Проверка авторизации пользователя
 if (!isset($_SESSION['login']) || !$_SESSION['login']) {
-    header("Location: ./authorization.html");
-    exit();
+header("Location: ./authorization.html");
+exit();
 }
 
 // Если форма отправлена
 if (isset($_POST['email'])) {
-    // Получаем новые значения данных из формы редактирования профиля
-    $name = mysqli_real_escape_string($conn, $_POST['name']);
-    $secondname = mysqli_real_escape_string($conn, $_POST['secondname']);
-    $email = mysqli_real_escape_string($conn, $_POST['email']);
-    $date = mysqli_real_escape_string($conn, $_POST['date']);
-    $about = mysqli_real_escape_string($conn, $_POST['about']);
+// Получаем новые значения данных из формы редактирования профиля
+$name = mysqli_real_escape_string($conn, $_POST['name']);
+$secondname = mysqli_real_escape_string($conn, $_POST['secondname']);
+$email = mysqli_real_escape_string($conn, $_POST['email']);
+$date = mysqli_real_escape_string($conn, $_POST['date']);
+$about = mysqli_real_escape_string($conn, $_POST['about']);
 
-    // Обновляем данные в базе данных
-    $sql = "UPDATE reg SET name='$name', secondname='$secondname', date='$date',about='$about' WHERE email='$email'";
-    if (mysqli_query($conn, $sql)) {
-        header("Location: ./profile.php");
-    } else {
-        echo "Ошибка при обновлении данных: " . mysqli_error($conn);
-    }
+// Обновляем данные в базе данных
+$sql = "UPDATE reg SET name='$name', secondname='$secondname', date='$date',about='$about' WHERE email='$email'";
+if (mysqli_query($conn, $sql)) {
+header("Location: ./profile.php");
+} else {
+echo "Ошибка при обновлении данных: " . mysqli_error($conn);
+}
 }
 
 // Получаем данные пользователя из базы данных для отображения в форме редактирования
@@ -36,46 +36,46 @@ mysqli_close($conn);
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Редактирование</title>
-    <link rel="stylesheet" href="./css/redstyle.css">
-    <script src="../script.js"></script>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Редактирование</title>
+<link rel="stylesheet" href="./css/redstyle.css">
+<script src="../script.js"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body>
 <div class="menu2">
-        <nav>
-        <ul>
-            <li> <button class="back" onclick="goBack()">Назад</button></li>
-        </ul>
-        </nav>
-        </div>
-    <div class="wrapper">
+<nav>
+<ul>
+<li> <button class="back" onclick="goBack()">Назад</button></li>
+</ul>
+</nav>
+</div>
+<div class="wrapper">
 <form method="post">
 <div class="mb-3">
-    <label for="name" class="form-label">Name</label>
-    <input type="text" class="col-xs-2" id="text" name="name"value="<?php echo $row['name']; ?>">
-  </div>
+<label for="name" class="form-label">Имя</label>
+<input type="text" class="col-xs-2" id="text" required name="name"value="<?php echo $row['name']; ?>">
+</div>
 
-  <div class="mb-3">
-  <label for="secondname" class="form-label">Имя</label>
-    <input type="text" class="col-xs-2"aria-describedby="emailHelp"name="secondname" value="<?php echo $row['secondname']; ?>">
+<div class="mb-3">
+<label for="secondname" class="form-label">Фамилия</label>
+<input type="text" class="col-xs-2" required name="secondname" value="<?php echo $row['secondname']; ?>">
 </div>
 <div class="mb-3">
 <label for="date">Дата рождения:</label>
-<input type="date" class="col-xs-2" id="date" name="date" value="<?php echo $row['date']; ?>">
+<input type="date" class="col-xs-2" id="date" required name="date" value="<?php echo $row['date']; ?>">
 </div>
 <div class="mb-3">
-  <label for="about" class="form-label">Расскажите о себе</label>
-    <input type="text" maxlength="100" class="col-xs-2" name="about" value="<?php echo $row['about']; ?>">
+<label for="about" class="form-label">Расскажите о себе</label>
+<input type="text" maxlength="100" required class="col-xs-2" name="about" value="<?php echo $row['about']; ?>">
 </div>
 <div class="mb-3">
-    <label for="email">Email:</label>
-    <input type="email" id="email" name="email" value="<?php echo $row['email']; ?>" readonly>
+<label for="email">Email:</label>
+<input type="email" id="email" required name="email" value="<?php echo $row['email']; ?>" readonly>
 </div>
-    <input type="submit" value="Обновить данные">
+<input type="submit" value="Обновить данные">
 </form>
 </div>
 </body>
