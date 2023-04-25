94222c719f265e6b8e11841e665f3bf7db2b9857
kaguhe <andreisichev123@gmail.com>
2023-04-25 07:58:11 +0300

    hahaha

--- oplata.php
+++ oplata.php
@@ -4,19 +4,23 @@ $name = $_POST['name'];
 $lastname = $_POST['lastname'];
 $email= $_POST['email'];
 $country= $_POST['country'];
-$city = $_POST['country'];
+$city = $_POST['city'];
 $address= $_POST['address'];
 $size= $_POST['size'];
-$stmt = mysqli_prepare($conn, "INSERT INTO reg (name,lastname,email,country,city,address,size) VALUES (?, ?, ?, ?,?,?)");
-mysqli_stmt_bind_param($stmt, 'sssssss', $name,$lastname,$email, $country,$city,$address,$size);
+$stmt = mysqli_prepare($conn, "INSERT INTO oplata (name,lastname,email,country,city,address,size) VALUES (?, ?, ?, ?, ?, ?, ?)");
+mysqli_stmt_bind_param($stmt, 'sssssss', $name, $lastname, $email, $country, $city, $address, $size);
 
 if (mysqli_stmt_execute($stmt)) {
     mysqli_stmt_close($stmt);
     mysqli_close($conn);
-    echo '<script>alert("Информация добавлена успешно."); window.location.href="../authorization.html";</script>';
+    echo '<script>alert("Информация добавлена успешно.")</script>';
     exit;
 } else {
-    echo "Ошибка: " . mysqli_error($conn);
+    $error_msg = "Ошибка: " . mysqli_error($conn);
     mysqli_stmt_close($stmt);
-    mysqli_close($conn);}
+    mysqli_close($conn);
+    echo '<script>alert("'.$error_msg.'")</script>';
+    exit;
+}
 ?>
+
