a21eae4a7a8d757cca975a5fe8d589021af1b43c
kaguhe <andreisichev123@gmail.com>
2023-04-25 12:24:57 +0300

    ihihih

--- menu.js
+++ menu.js
@@ -9,16 +9,4 @@ menuIcon.onclick = function(e) {
 	navigation.classList.toggle('dark');
 }
 
-   const products = document.querySelectorAll('.product');
 
-   products.forEach(product => {
-	 const price = product.getAttribute('data-price');
-	 const description = product.querySelector('.description').textContent;
-	 const priceInput = document.querySelector('input[name="price"]');
-	 const descInput = document.querySelector('input[name="desc"]');
-   
-	 product.addEventListener('click', () => {
-	   priceInput.value = price;
-	   descInput.value = description;
-	 });
-   });
--- oplata.php
+++ oplata.php
@@ -13,16 +13,11 @@ $stmt = mysqli_prepare($conn, "INSERT INTO oplata (name,lastname,email,country,c
 mysqli_stmt_bind_param($stmt, 'sssssssss', $name, $lastname, $email, $country, $city, $address, $size,$price,$desc);
 
 if (mysqli_stmt_execute($stmt)) {
-    mysqli_stmt_close($stmt);
-    mysqli_close($conn);
     echo '<script>alert("Информация добавлена успешно.")</script>';
-    exit;
 } else {
     $error_msg = "Ошибка: " . mysqli_error($conn);
-    mysqli_stmt_close($stmt);
-    mysqli_close($conn);
     echo '<script>alert("'.$error_msg.'")</script>';
-    exit;
 }
+mysqli_close($conn);
 ?>
 
