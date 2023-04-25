fad84e7e142836b24afb582e675b105d9a429d03
kaguhe <andreisichev123@gmail.com>
2023-04-25 09:31:52 +0300

    hahaha

--- oplata.php
+++ oplata.php
@@ -7,8 +7,10 @@ $country= $_POST['country'];
 $city = $_POST['city'];
 $address= $_POST['address'];
 $size= $_POST['size'];
-$stmt = mysqli_prepare($conn, "INSERT INTO oplata (name,lastname,email,country,city,address,size) VALUES (?, ?, ?, ?, ?, ?, ?)");
-mysqli_stmt_bind_param($stmt, 'sssssss', $name, $lastname, $email, $country, $city, $address, $size);
+$price =$_POST['price'];
+$desc= $_POST['desc'];
+$stmt = mysqli_prepare($conn, "INSERT INTO oplata (name,lastname,email,country,city,address,size,price,description) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
+mysqli_stmt_bind_param($stmt, 'sssssssss', $name, $lastname, $email, $country, $city, $address, $size,$price,$desc);
 
 if (mysqli_stmt_execute($stmt)) {
     mysqli_stmt_close($stmt);
