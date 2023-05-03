ab26f18fc050bd63b6b58fc2f32c68229ab3a7c8
kaguhe <107109714+therken@users.noreply.github.com>
2023-05-03 07:28:24 +0300

    Update menu.js

--- script/menu.js
+++ script/menu.js
@@ -22,32 +22,3 @@ products.forEach(product => {
 	descInput.value = description;
   });
 });
-const checkoutForm = document.getElementById('checkout-form');
-
-const cartRows = document.querySelectorAll('.product-row');
-const priceInput = document.querySelector('input[name="price"]');
-const descriptionInput = document.querySelector('input[name="description"]');
-
-let totalPrice = 0;
-let cartItems = '';
-
-cartRows.forEach(row => {
-  const imageSrc = row.querySelector('.cart-image').getAttribute('src');
-  const description = row.querySelector('.description').textContent;
-  const price = row.querySelector('.cart-price').textContent;
-  const quantity = row.querySelector('.product-quantity').value;
-  const size = row.querySelector('.size').value;
-
-  const itemPrice = parseFloat(price.replace('$', ''));
-  const itemTotalPrice = itemPrice * quantity;
-
-  totalPrice += itemTotalPrice;
-
-  cartItems += `${description} (${quantity} x ${price} - Размер: ${size})\n`;
-
-});
-
-priceInput.value = totalPrice.toFixed(2);
-descriptionInput.value = cartItems;
-
-checkoutForm.submit();
