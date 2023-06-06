2e96e78f8e315fa92ff9830232e4e2fc75a1d881
зеркен <107109714+therken@users.noreply.github.com>
2023-06-06 10:18:50 +0300

    yesss

---
+++ script/sort.js
@@ -0,0 +1,36 @@
+// Функция для сортировки товаров по цвету
+function sortProductsByColor() {
+    // Получаем все товары
+    var productsContainer = document.getElementById("all");
+    var products = productsContainer.getElementsByClassName("product");
+  
+    // Получаем выбранные цвета
+    var selectedColors = [];
+    var checkboxes = document.querySelectorAll("input[type='checkbox'][name='color']");
+    
+    // Проходимся по всем флажкам и добавляем выбранные цвета в массив
+    for (var i = 0; i < checkboxes.length; i++) {
+      if (checkboxes[i].checked) {
+        selectedColors.push(checkboxes[i].getAttribute("data-color"));
+      }
+    }
+  
+    // Проходимся по всем товарам и скрываем или показываем их в зависимости от выбранных цветов
+    for (var j = 0; j < products.length; j++) {
+      var product = products[j];
+      var productColor = product.getAttribute("data-color");
+      
+      if (selectedColors.length === 0 || selectedColors.includes(productColor)) {
+        product.style.display = "block"; // Показываем товар
+      } else {
+        product.style.display = "none"; // Скрываем товар
+      }
+    }
+  }
+  
+  // Находим все флажки по имени и назначаем обработчик события изменения состояния
+  var checkboxes = document.querySelectorAll("input[type='checkbox'][name='color']");
+  for (var k = 0; k < checkboxes.length; k++) {
+    checkboxes[k].addEventListener("change", sortProductsByColor);
+  }
+  
