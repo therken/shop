7a5f337c8af97dd8a4ac506ff6b95520dac95771
1W3UxNt <120009117+1W3UxNt@users.noreply.github.com>
2023-06-08 12:09:05 +0600

    Update oplata.php

--- card/oplata.php
+++ card/oplata.php
@@ -1,24 +1,20 @@
-<?php
-include ('../profile/php/setting.php');
-$name = $_POST['name'];
-$lastname = $_POST['lastname'];
-$email= $_POST['email'];
-$country= $_POST['country'];
-$city = $_POST['city'];
-$address= $_POST['address'];
-$size= $_POST['size'];
-$price =$_POST['price'];
-$desc= $_POST['desc'];
-$stmt = mysqli_prepare($conn, "INSERT INTO oplata (name,lastname,email,country,city,address,size,price,description) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
-mysqli_stmt_bind_param($stmt, 'sssssssss', $name, $lastname, $email, $country, $city, $address, $size,$price,$desc);
-
-if (mysqli_stmt_execute($stmt)) {
-    echo '<script>alert("Заказ добавлен"); window.location.href="./home.html";</script>';
-} else {
-    $error_msg = "Ошибка: " . mysqli_error($conn);
-    echo '<script>alert("'.$error_msg.'")</script>';
-}
-mysqli_close($conn);
+<?php 
+include ('../profile/php/setting.php'); 
+$name = $_POST['name']; 
+$surname = $_POST['surname']; 
+$card = $_POST['card']; 
+$email= $_POST['email']; 
+$country= $_POST['country']; 
+$city = $_POST['city']; 
+$address= $_POST['address']; 
+$stmt = mysqli_prepare($conn, "INSERT INTO card (name,surname,card,email,country,city,address) VALUES (?, ?, ?, ?, ?, ?, ?)"); 
+mysqli_stmt_bind_param($stmt, 'sssssss', $name, $surname, $card, $email, $country, $city, $address); 
+ 
+if (mysqli_stmt_execute($stmt)) { 
+    echo '<script>alert("Заказ добавлен"); window.location.href="../home.html";</script>'; 
+} else { 
+    $error_msg = "Ошибка: " . mysqli_error($conn); 
+    echo '<script>alert("'.$error_msg.'")</script>'; 
+} 
+mysqli_close($conn); 
 ?>
-
-
